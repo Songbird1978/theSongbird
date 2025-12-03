7ba46cf765296b4c9150756b5a7c1096ddc463dc
Songbird1978 <118288349+Songbird1978@users.noreply.github.com>
2025-12-03 10:01:13 +0000

    added songbirdSites.css

--- client/src/pages/hotspots/songbirdDesigns.jsx
+++ client/src/pages/hotspots/songbirdDesigns.jsx
@@ -47,40 +47,6 @@ function SongbirdDesigns() {
         >
             <div className="designContainer w-full flex justify-center bg-stone-200 opacity-80">
                 <h1 className="topicTitle">Songbird Designs</h1>
-
-                <motion.div
-                    className="grid p-10 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-items-center"
-                    variants={containerVariants}
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true }}
-                >
-                    {records.map(() => (
-                        <motion.div key={record.id} variants={itemVariants}>
-                            <Card
-                                key={record.id}
-                                className="max-w-60 flex-col mb-10"
-                            >
-                                <CardContent>
-                                    <img
-                                        src={record.imageUrl[0]}
-                                        alt={record.title}
-                                        className="w-full h-50"
-                                    />
-                                    <p className="font-gothic optionTitle text-left text-md">
-                                        {record.title}
-                                    </p>{" "}
-                                    {/* ← Artist info available! */}
-                                </CardContent>
-                                <CardHeader>
-                                    <CardTitle className="font-gothic text-left optionTitle text-sm">
-                                        by {record.artist.name}
-                                    </CardTitle>
-                                </CardHeader>
-                            </Card>
-                        </motion.div>
-                    ))}
-                </motion.div>
             </div>
         </motion.div>
     );
--- client/src/pages/hotspots/songbirdSites.jsx
+++ client/src/pages/hotspots/songbirdSites.jsx
@@ -1,6 +1,5 @@
-import './hotspots.css';
-import { motion } from 'framer-motion';
-
+import "./hotspots.css";
+import { motion } from "framer-motion";
 
 function SongbirdSites() {
     return (
@@ -11,9 +10,94 @@ function SongbirdSites() {
             exit={{ opacity: 0 }}
             transition={{ duration: 0.5 }}
         >
-            <div className="hotspotContainer">
+            <motion.div
+                className="backdrop scrollbar-hide"
+                initial={{ y: 500 }}
+                transition={{ duration: 1.5, ease: "easeIn" }}
+                animate={{ y: 0, height: "auto" }}
+            >
+                <Greetings setComplete={setComplete} />
+                {complete && (
+                    <motion.div className="ogruCardContainer">
+                        {optionCards.map(
+                            ({ id, label, color, colorTwo, p }) => (
+                                <OgruOptionCard
+                                    key={id}
+                                    id={id}
+                                    label={label}
+                                    p={p}
+                                    color={color}
+                                    colorTwo={colorTwo}
+                                    onClick={() => {
+                                        setClicked(id);
+                                        setTimeout(() => {
+                                            document
+                                                .getElementById(
+                                                    "contentSection"
+                                                )
+                                                .scrollIntoView({
+                                                    behavior: "smooth",
+                                                });
+                                        }, 200); // small delay to allow animation if needed
+                                    }}
+                                />
+                            )
+                        )}
+                    </motion.div>
+                )}
+                <motion.div id="contentSection" className="mt-10 optionPage">
+                    <AnimatePresence mode="wait">
+                        {clicked === "ogru" && (
+                            <motion.div
+                                key="ogru"
+                                initial={{ opacity: 0, y: 50 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0, y: -50 }}
+                                transition={{ duration: 0.5 }}
+                            >
+                                <Ogru />
+                            </motion.div>
+                        )}
+                        {clicked === "listen" && (
+                            <motion.div
+                                key="listen"
+                                initial={{ opacity: 0, y: 50 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0, y: -50 }}
+                                transition={{ duration: 0.5 }}
+                            >
+                                <Listen />
+                            </motion.div>
+                        )}
+                        {clicked === "discover" && (
+                            <motion.div
+                                key="discover"
+                                initial={{ opacity: 0, y: 50 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0, y: -50 }}
+                                transition={{ duration: 0.5 }}
+                            >
+                                <Discover />
+                            </motion.div>
+                        )}
+                    </AnimatePresence>
+                </motion.div>
+                <div className="magicLeafDiv">
+                    <Button
+                        className="magicLeafBack"
+                        id="magicLeafText"
+                        text="Exit"
+                        to={"/townscene"}
+                        style={{ cursor: "pointer" }}
+                    ></Button>
+                </div>
+            </motion.div>
+            <div className="sitesContainer">
                 <h1>SongbirdSites</h1>
-                <p>This is the page for SongbirdSites. Here you can see my music work, albums, etc.</p>
+                <p>
+                    This is the page for SongbirdSites. Here you can see my
+                    music work, albums, etc.
+                </p>
             </div>
         </motion.div>
     );
