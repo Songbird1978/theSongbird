c9af1811fac235a3b24458f742d4ec94e1e155c6
Songbird1978 <118288349+Songbird1978@users.noreply.github.com>
2026-01-19 11:27:16 +0000

    dummy

--- client/src/components/bird2.jsx
+++ client/src/components/bird2.jsx
@@ -1,47 +1,62 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useEffect } from "react";
+import banner from "../assets/birdBanner.png";
+import "../pages/townscene/townscene.css";
+import birdFlying from "../assets/birdFlying.mp3";
 
-import { motion, AnimatePresence } from 'framer-motion';
-import { useEffect } from 'react';
-import banner from '../assets/birdBanner.png';
-import '../pages/townscene/townscene.css';
-import birdFlying from '../assets/birdFlying.mp3';
-
-function Bird2 () {
-
+function Bird2() {
+    //tr
     useEffect(() => {
         const flying = new Audio(birdFlying);
         flying.loop = true;
         flying.volume = 0.4;
-        flying.preload = 'auto';
+        flying.preload = "auto";
         flying.play();
-// cleanup function - runs when component unmounts
-    return () => {
-        flying.pause();
-        flying.currentTime = 0; //reset to beginning 
-};
-}, []);
-  
+        // cleanup function - runs when component unmounts
+        return () => {
+            flying.pause();
+            flying.currentTime = 0; //reset to beginning
+        };
+    }, []);
+
     return (
-            <AnimatePresence>
-                <motion.div 
-                style={{ height: 'auto', position: 'absolute', top: '10%', zIndex: '9999' }}
+        <AnimatePresence>
+            <motion.div
+                style={{
+                    height: "auto",
+                    position: "absolute",
+                    top: "10%",
+                    zIndex: "9999",
+                }}
                 initial={{
-                    x: 'calc(100vw + 400px)',
+                    x: "calc(100vw + 400px)",
                 }}
                 animate={{
                     x: "-600px",
-                    rotate: [0, 0, 0,  0.1, 0.2, 0.3, 0.4, 0.5, 0.4, 0.3, 0.2, 0.1, 0, 0, 0, 0, 0]
+                    rotate: [
+                        0, 0, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.4, 0.3, 0.2, 0.1, 0,
+                        0, 0, 0, 0,
+                    ],
                 }}
                 transition={{
-                    rotate: { duration: 0.5, repeat: Infinity, repeatType: 'reverse' },
-                    x: { duration: 20, ease: "linear", repeat: Infinity  }
+                    rotate: {
+                        duration: 0.5,
+                        repeat: Infinity,
+                        repeatType: "reverse",
+                    },
+                    x: { duration: 20, ease: "linear", repeat: Infinity },
                 }}
                 alt="bird with instructions banner"
                 className="birdBanner"
-                >                
-                <img src={banner} alt="bird carrying banner with instructions for use - use keyboard arrows or mouse to explore the town" style={{ width: 600}} />
-                </motion.div>
-            </AnimatePresence>
-    )
+            >
+                <img
+                    src={banner}
+                    alt="bird carrying banner with instructions for use - use keyboard arrows or mouse to explore the town"
+                    style={{ width: 600 }}
+                />
+            </motion.div>
+        </AnimatePresence>
+    );
 }
 
 export default Bird2;
