1553281e245a55c14954338e533d3157fc5a880b
Songbird1978 <118288349+Songbird1978@users.noreply.github.com>
2026-01-27 08:50:19 +0000

    changed read in sites

--- client/src/pages/hotspots/read.jsx
+++ client/src/pages/hotspots/read.jsx
@@ -4,7 +4,8 @@ import { motion } from "framer-motion";
 import Typewriter from "../../components/typewriter.jsx";
 
 function Read() {
-    {/*
+    {
+        /*
     const [typingComplete, setTypingComplete] = useState(false);
 
     useEffect(() => {
@@ -14,7 +15,8 @@ function Read() {
             setTypingComplete(true);
         }
     }, [typingComplete]);
-*/}
+*/
+    }
     return (
         <motion.div
             className="page"
@@ -33,7 +35,7 @@ function Read() {
                         "I am a musician (piano & voice) and I trained classically throughout my youth, and studied Music & Sound Technology at University. I have always been involved in music creation and performance. I then trained as a music teacher in 2007 and taught at all levels, primary to level 5 at schools & colleges.",
                         "After Covid and some soul-searching, I decided to move away from teaching and change career. I enrolled on a Front-End Development Bootcamp. Web Development and design has always been an interest and I have dabbled over the years, but I didn't get the opportunity to follow that pathway until now.",
                         "Since completing a Front-end Web Development bootcamp, I have continued to develop, adding some understanding of backend development, databases, accessibility and new technologies to my array of skills.",
-                        "I have included projects here that have led to employment and I would dearly love to work in a web development team and develop more skills and knowledge in the future.",
+                        "I have included commercial projects here for you to browse and I would dearly love to work for a mission-driven organisation, building for social impact, in a web development team. ",
                     ]}
                     speed={50}
                     pauseBetween={1000}
